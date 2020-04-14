e1dd0d1e43591e76731379c02048d5a7804e0cf9
Lucas Palavecino <lucaspalavecino46@gmail.com>
2020-04-14 11:28:29 -0300

    first commit

---
+++ app/src/main/java/com/cursoandroid/gestordegastos/views/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cursoandroid.gestordegastos.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cursoandroid.gestordegastos.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
